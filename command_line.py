c3ab61e123fed4a649a20f26f4e6b2839095a66f
beyonce <beyonce.fabian@gmail.com>
2026-01-25 11:15:57 -0600

    adding command_line and test_command_line

--- command_line.py
+++ command_line.py
@@ -1,4 +1,36 @@
 '''
-The eventual location for the command line interface (CLI) for the project.
-This will be the entry point for the project when run from the command line.
+Purpose: Allows user to interact with data with the command line
+
 '''
+import csv
+import sys
+import argparse
+
+def average_co2 (country):
+    pass
+
+
+def year_co2 (year):
+    '''
+    
+    '''
+    pass
+
+def highest_co2 (country):
+    '''
+    Purpose: Return the highest co2 emissions for a country 
+    
+    '''
+    pass
+
+def load_data():
+    '''
+    Purpose: Load data for other functions in this file
+    '''
+    pass
+
+def main ():
+    '''
+    Purpose: 
+    '''
+    pass
