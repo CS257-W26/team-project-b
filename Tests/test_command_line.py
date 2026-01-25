713b531573565a62ea114126b285bb2cd9b3722a
BradleyHunde <hundeb@carleton.edu>
2026-01-25 11:00:57 -0600

    Added command_line.py and test_command_line.py

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -1,1 +1,6 @@
 import unittest
+from command_line import average_co2,year_co2,highest_co2,load_data,main
+
+class TestCommandLine (unittest.TestCase):
+    def test_average_co2(self):
+        self.assertEqual ()
