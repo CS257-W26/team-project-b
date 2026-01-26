a5edc88bd42cc154919d67b5805ee14357d56f3d
BradleyHunde <hundeb@carleton.edu>
2026-01-26 14:39:18 -0600

    Changes to load_data and test_load_data

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -2,12 +2,12 @@ import unittest
 from command_line import average_co2,year_co2,highest_co2,load_data,main
 
 class TestCommandLine (unittest.TestCase):
-    def test_load_data (self):
+    def test_load_data(self):
         '''Arguments: self (TestCommandLine)
         Return: none
         Purpose: Tests load data with the dummy_data.csv file
         '''
-        self.assertEqual (load_data('Data/dummy_data.csv'), [['country', 'year', 'cumulative_co2'], 
+        self.assertEqual(load_data('Dummy/dummy_data.csv'), [['country', 'year', 'cumulative_co2'], 
         ['Canada', '2004', '1.452'],
         ['Canada', '1998', '2.045'],
         ['Canada', '2018', '3.192'],
@@ -24,8 +24,8 @@ class TestCommandLine (unittest.TestCase):
         Return: none
         Purpose: Tests the average_co2 function in command_line.py
         '''
-        self.assertEqual (average_co2('Canada'),2.22966667)
-        self.assertEqual (average_co2('Japan'),10.95900)
+        self.assertEqual(average_co2('Canada'),2.22966667)
+        self.assertEqual(average_co2('Japan'),10.95900)
 
     def test_average_co2_edge(self):
         '''Arguments: self (TestCommandLine)
@@ -35,7 +35,7 @@ class TestCommandLine (unittest.TestCase):
         self.assertEqual(average_co2(12), 'Please input a string for a country')
         self.assertEqual(average_co2(''), 'Please input a valid country')
 
-    def test_year_co2 (self):
+    def test_year_co2(self):
         '''Arguments: self (TestCommandLine)
         Return: none
         Purpose: Tests the year_co2 function in command_line.py
@@ -43,7 +43,7 @@ class TestCommandLine (unittest.TestCase):
         self.assertEqual(year_co2('2004'), ['Canada', '2004', '1.452'], ['Japan', '2004', '1.133'], ['Argentina', '2004', '0.630'])
         self.assertEqual(year_co2('1998'), ['Canada', '1998', '2.045'], ['Japan', '1998', '0.792'], ['Argentina', '2004', '1.582'])
 
-    def test_year_co2 (self):
+    def test_year_co2(self):
         '''Arguments: self (TestCommandLine)
         Return: none
         Purpose: Tests edge cases for the year_co2 function in command_line.py
