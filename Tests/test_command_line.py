cfafbf0e339700bcda0ac4cd17db94488e97cd79
BradleyHunde <hundeb@carleton.edu>
2026-01-26 17:38:05 -0600

    Added a new col to dummy_data and started on ratio function

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -10,34 +10,35 @@ class TestCommandLine(unittest.TestCase):
         Purpose: Tests load data with the dummy_data.csv file
         '''
         
-        self.assertEqual(load_data('Data/dummy_data_one_line.csv'),[['country', 'year', 'cumulative_co2']])
-        self.assertEqual(load_data('Data/dummy_data.csv'), [['country', 'year', 'cumulative_co2'],['Canada', '2004', '1.452'],
-        ['Canada', '1998', '2.045'],
-        ['Canada', '2018','3.192'],
-        ['Japan', '2004', '1.133'],
-        ['Japan', '1998', '0.792'],
-        ['Japan', '2018', '9.034'],
-        ['Argentina', '2004', '0.630'],
-        ['Argentina', '1998', '1.582'],
-        ['Argentina', '2018', ''],
-        ['Argentina', '2019', '1.609']])
+        self.assertEqual(load_data('Data/dummy_data_one_line.csv'),[['country','year','cumulative_co2']])
+        self.assertEqual(load_data('Data/dummy_data.csv'), [['country','year','cumulative_co2','co2_per_capita'],
+        ['Canada','2004','1.452','12.345'],
+        ['Canada','1998','2.045','10.432'],
+        ['Canada','2018','3.192','15.725'],
+        ['Japan','2004','1.133','12.333'],
+        ['Japan','1998','0.792','8.324'],
+        ['Japan','2018','9.034','20.324'],
+        ['Argentina','2004','0.630','1.234'],
+        ['Argentina','1998','1.582','9.87'],
+        ['Argentina','2018','',''],
+        ['Argentina','2019','1.609','10.23']])
     
     def test_average(self):
         '''Arguments: self (TestCommandLine)
         Return: none
         Purpose: Tests the average_co2 function in command_line.py
         '''
-        self.assertAlmostEqual(average('Canada','Data/dummy_data.csv'),2.22966667)
-        self.assertAlmostEqual(average('Japan','Data/dummy_data.csv'),3.65300)
-        self.assertAlmostEqual(average('Argentina','Data/dummy_energy_data.csv'),1.27366667)
+        self.assertAlmostEqual(average('Canada','Data/dummy_data.csv',2),2.22966667)
+        self.assertAlmostEqual(average('Japan','Data/dummy_data.csv',2),3.65300)
+        self.assertAlmostEqual(average('Argentina','Data/dummy_energy_data.csv',2),1.27366667)
 
     def test_average_edge(self):
         '''Arguments: self (TestCommandLine)
         Return: none
         Purpose: Tests edge cases for the average_co2 function in command_line.py
         '''
-        self.assertEqual(average(12,'Data/dummy_data.csv'), 'Please input a string for a country')
-        self.assertEqual(average('','Data/dummy_data.csv'), 'Please input a valid country')
+        self.assertEqual(average(12,'Data/dummy_data.csv',2), 'Please input a string for a country')
+        self.assertEqual(average('','Data/dummy_data.csv',2), 'Please input a valid country')
 
     def test_year_co2(self):
         '''Arguments: self (TestCommandLine)
