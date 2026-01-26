74774893b6e02a13d62ae5d226436990b2ac5a8f
beyonce <beyonce.fabian@gmail.com>
2026-01-26 16:12:48 -0600

    staging changes

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -1,5 +1,5 @@
 import unittest
-from command_line import average_co2,year_co2,highest_co2,load_data,main
+from command_line import average_co2,year_co2,highest_biofuel_consumption,load_data,main
 
 class TestCommandLine (unittest.TestCase):
     def test_load_data(self):
