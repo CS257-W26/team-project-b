49583712ada3721d463a2da8a4bb141498ba1e9e
BradleyHunde <hundeb@carleton.edu>
2026-01-26 13:53:28 -0600

    added tests for year_co2

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -3,16 +3,17 @@ from command_line import average_co2,year_co2,highest_co2,load_data,main
 
 class TestCommandLine (unittest.TestCase):
     def test_load_data (self):
-        self.assertEqual (load_data('dummy_data.csv'), [['country', 'year', 'cumulative_co2'], ['Canada', 2004, 1.452],
-        ['Canada', 1998, 2.045],
-        ['Canada', 2018, 3.192],
-        ['Japan', 2000, 1.133],
-        ['Japan', 1979, 0.792],
-        ['Japan', 1995, 9.034],
-        ['Argentina', 1994, 0.630],
-        ['Argentina', 2008, 1.582],
-        ['Argentina', 2013, ],
-        ['Argentina', 2017, 1.609]])
+        self.assertEqual (load_data('Data/dummy_data.csv'), [['country', 'year', 'cumulative_co2'], 
+        ['Canada', '2004', '1.452'],
+        ['Canada', '1998', '2.045'],
+        ['Canada', '2018', '3.192'],
+        ['Japan', '2000', '1.133'],
+        ['Japan', '1979', '0.792'],
+        ['Japan', '1995', '9.034'],
+        ['Argentina', '1994', '0.630'],
+        ['Argentina', '2008', '1.582'],
+        ['Argentina', '2013', ''],
+        ['Argentina', '2017', '1.609']])
     
     def test_average_co2(self):
         self.assertEqual (average_co2('Canada'),2.22966667)
@@ -21,3 +22,11 @@ class TestCommandLine (unittest.TestCase):
     def test_average_co2_edge(self):
         self.assertEqual(average_co2(12), 'Please input a string for a country')
         self.assertEqual(average_co2(''), 'Please input a valid country')
+
+    def test_year_co2 (self):
+        self.assertEqual(year_co2('2004'), ['Canada', '2004', '1.452'], ['Japan', '2004', '1.133'], ['Argentina', '2004', '0.630'])
+        self.assertEqual(year_co2('1998'), ['Canada', '1998', '2.045'], ['Japan', '1998', '0.792'], ['Argentina', '2004', '1.582'])
+
+    def test_year_co2 (self):
+        self.assertEqual(year_co2('Canada'), 'Please input a valid year')
+        self.assertEqual(year_co2('98'), 'Please input a valid year')
