1fbd11c0161ff7e859c9de216990a194f5edf2e0
BradleyHunde <hundeb@carleton.edu>
2026-01-30 09:19:54 -0600

    Fixes to class and object usage

--- ProductionCode/core.py
+++ ProductionCode/core.py
@@ -1,29 +1,31 @@
-class Features(self):
+import csv
+
+class Features():
 
     data=[]
 
-    def load_data(dataset):
+    def load_data(self,dataset):
         '''
         Purpose: Load data for other functions in this file
         '''
-        data.clear()
+        self.data.clear()
         with open(dataset, newline='', encoding='utf-8') as file:
             reader = csv.reader(file)
             for row in reader:
-                data.append(row)
-        return data
+                self.data.append(row)
+        return self.data
 
-    def average(country,dataset,col):
+    def average(self,country,dataset,col):
         '''Arguments: country (string)
         Return: The average CO2 emissions of a country (float), 
         or a correction of how this function should work (string)
         Purpose: To get the average CO2 emissions of a country
         '''
-        load_data(dataset)
+        self.load_data(dataset)
         total = 0
         count = 0
         if isinstance(country, str):
-            for row in data:
+            for row in self.data:
                 if (row[0] == country) and (row[col] != ''):
                     total += float(row[col])
                     count += 1
@@ -33,29 +35,29 @@ class Features(self):
             return 'Please input a valid country'
         return float (total/count)
 
-    def ratio(country):
+    def ratio(self,country):
         '''Arguments: country (year)
         Return: A ratio (float) 
         Purpose: Calculates the ratio for co2 and co2 per capita
         '''
-        avg_co2 = average(country,'Data/dummy_data.csv',3)
-        avg_energy = average(country,'Data/dummy_energy_data.csv',3)
+        avg_co2 = self.average(country,'Data/dummy_data.csv',3)
+        avg_energy = self.average(country,'Data/dummy_energy_data.csv',3)
         ratio_variable = avg_co2/avg_energy
         return ratio_variable
 
-    def year_co2 (year):
+    def year_co2 (self,year):
         '''Arguments: year (string)
         Return: A list of lists (string) with each country and
         total CO2 emissions from a specific year
         Purpose: To get the total CO2 emissions of each country
         in the dataset from a specific year
         '''
-        load_data("Data/dummy_energy_data.csv")
+        self.load_data("Data/dummy_energy_data.csv")
 
         output = []
 
         if isinstance(year, str):
-            for row in data:
+            for row in self.data:
                 country = row[0]
                 year_row = row[1]
                 co2 = row[2]
@@ -67,7 +69,7 @@ class Features(self):
             return "Please input a valid year"
         return output
 
-    def highest_biofuel_consumption(country):
+    def highest_biofuel_consumption(self,country):
         '''Argument: country (String)
         Return: int highest emissions
         Purpose: Returns a single int representing the highest
@@ -75,7 +77,7 @@ class Features(self):
         '''
         biofuel = 0
         if isinstance(country, str):
-            for row in load_data("Data/dummy_energy_data.csv"):
+            for row in self.load_data("Data/dummy_energy_data.csv"):
                 if (row[0] == country and row[2] != ''):
                     biofuel = max(biofuel, float(row[2]))
             return biofuel
--- command_line.py
+++ command_line.py
@@ -2,8 +2,8 @@
 Purpose: Allows user to interact with data with the command line
 
 '''
-import csv
 import sys
+from ProductionCode.core import Features
 
 def main():
     '''Arguments: none
@@ -11,16 +11,17 @@ def main():
     Purpose: Takes command line inputs to run other functions in command_line
     '''
     args = sys.argv
+    core = Features()
 
     if len(args) == 1:
         print("Usage: python3 command_line.py [options]")
     elif len(args) == 3:
         if args[1] == 'ratio':
-            print (ratio(args[2]))
+            print (core.ratio(args[2]))
         if args[1] == 'year_co2':
-            print (year_co2(args[2]))
+            print (core.year_co2(args[2]))
         if args[1] == 'biofuel':
-            print (highest_biofuel_consumption(args[2]))
+            print (core.highest_biofuel_consumption(args[2]))
     return 'Invalid inputs'
 
 if __name__ == "__main__":
