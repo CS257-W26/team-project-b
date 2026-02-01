84333bd2ad61c6c5ef12f6d6e64419abefd4df87
beyonce <beyonce.fabian@gmail.com>
2026-02-01 10:41:00 -0600

    working on argsparse

--- command_line.py
+++ command_line.py
@@ -2,27 +2,48 @@
 Purpose: Allows user to interact with data with the command line
 
 '''
-import sys
+import argparse
 from ProductionCode.core import Features
 
-def main():
-    '''Arguments: none
-    Return value: none
-    Purpose: Takes command line inputs to run other functions in command_line
+def parsing():
+    ''''Arguments: none
+    Return value: parser
+    Purpose: Taking command line inputs to run functions in command_line
     '''
-    args = sys.argv
-    core = Features()
+    parser = argparse.ArgumentsParser(
+        prog = 'command_line.py'
+        epilog = (
+            "Example Commands:\n"
+            "python3 command_line.py --ratio Japan\n"
+            "python3 command_line.py --year_co2 2004\n"
+            "python3 command_line.py --biofuel Canada\n"
 
-    if len(args) == 1:
-        print("Usage: python3 command_line.py [options]")
-    elif len(args) == 3:
-        if args[1] == 'ratio':
-            print (core.ratio(args[2]))
-        if args[1] == 'year_co2':
-            print (core.year_co2(args[2]))
-        if args[1] == 'biofuel':
-            print (core.highest_biofuel_consumption(args[2]))
-    return 'Invalid inputs'
+        )
+    )
+
+    parser.add_argument('-a', '--average', type = str, help= 'Provides')
+    parser.add_argument('-r', '--ratio', type = str, help = '')
+    parser.add_argument('-y', '--year_co2', type = str, help = '')
+    parser.add_argument('-b', '--biofuel', type = str, help = '' )
+
+# def main():
+#     '''Arguments: none
+#     Return value: none
+#     Purpose: Takes command line inputs to run other functions in command_line
+#     '''
+#     args = sys.argv
+#     core = Features()
+
+#     if len(args) == 1:
+#         print("Usage: python3 command_line.py [options]")
+#     elif len(args) == 3:
+#         if args[1] == 'ratio':
+#             print (core.ratio(args[2]))
+#         if args[1] == 'year_co2':
+#             print (core.year_co2(args[2]))
+#         if args[1] == 'biofuel':
+#             print (core.highest_biofuel_consumption(args[2]))
+#     return 'Invalid inputs'
 
 if __name__ == "__main__":
     main()
