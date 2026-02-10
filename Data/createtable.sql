7b1b5a60d3d27bfda734d1d788a3aef833f8be49
BradleyHunde <hundeb@stearns.mathcs.carleton.edu>
2026-02-10 15:46:02 -0500

    Updated createtable and datasource

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -1,16 +1,50 @@
-DROP IF EXISTS years;
+DROP TABLE IF EXISTS years,countries,co2,energy,co2_data;
 CREATE TABLE years (
     id SERIAL,
-    years INTEGER,
+    year INTEGER,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE countries(
+    id SERIAL,
+    country TEXT,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE co2 (
-    country TEXT,
-    
-    co2_capita INTEGER,
-    
-    FOREIGN KEY (years_id)
-)
+    id SERIAL,
+    co2_per_capita FLOAT,
+    cummulative_co2 FLOAT,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE 
+CREATE TABLE energy (
+    id SERIAL,
+    biofuel_cons_per_capita FLOAT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE co2_data (
+    country TEXT,
+    year INTEGER,
+    iso_code TEXT,
+    population TEXT,
+    gdp TEXT,
+    cement_co2 FLOAT,
+    co2 FLOAT,
+    co2_growth_abs FLOAT,
+    co2_including_luc FLOAT,
+    co2_per_capita FLOAT,
+    co2_per_gdp FLOAT,
+    co2_per_unit_energy FLOAT,
+    coal_co2 FLOAT,
+    consumption_co2 FLOAT,
+    cumulative_co2 FLOAT,
+    flaring_co2 FLOAT,
+    gas_co2 FLOAT,
+    land_use_change_co2 FLOAT,
+    methane FLOAT,
+    nitrous_oxide FLOAT,
+    oil_co2 FLOAT,
+    other_industry_co2 FLOAT
+);
--- ProductionCode/datasource.py
+++ ProductionCode/datasource.py
@@ -8,4 +8,10 @@ class DataSource:
         connect = f"postgresql://{config.USER}:{config.PASSWORD}@localhost:5432/{config.DATABASE}"
         self.db = records.Database(connect)
 
-    def 
+    def get_country (self,data,country):
+        result = self.db.query(f'SELECT * FROM {data} WHERE country = {country}')
+        return result.export('csv')
+
+    def get_year (self,data,year):
+        result = self.db.query(f'SELECT * FROM {data} WHERE years = {year}')
+        return result.export('csv')
