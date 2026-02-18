9dd53c0635ea50dd1a3bc244f879c8976f3894cd
beyonce <beyonce.fabian@gmail.com>
2026-02-18 14:51:25 -0600

    returned datasource functions as they were

--- ProductionCode/datasource.py
+++ ProductionCode/datasource.py
@@ -69,7 +69,7 @@ class DataSource:
         Purpose: Gets the biofuel_consumption data from energy_data for a specified country
         Return: A csv
         '''
-        result = self.db.query(f"SELECT MAX(biofuel_consumption) FROM energy_data WHERE country = '{country}'")
+        return self.get_value('energy_data', 'biofuel_consumption', country)
 
     def get_co2_per_capita (self, country):
         '''Arguments: self, country
