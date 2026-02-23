c1b2b064daa9d17e67ab61bd071b2f83825588c8
beyonce-m <maldonadob@carleotn.edu>
2026-02-22 21:25:23 -0500

    patched tests for command_line.py

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -20,43 +20,62 @@ class TestCommandLine(unittest.TestCase):
         main()
         return sys.stdout.getvalue().strip()
 
-    def test_main(self):
-        '''Arguments: self (TestProductionCode)
+    @patch('command_line.core.ratio')
+    def test_arg_ratio(self, mock_ratio):
+        '''Arguments: self, mock_ratio
         Return value: None
         Purpose: Tests whether the main function returns the correct value for the
         specified function in command line arguments
         '''
-        #Testing for arg_ratio
+        #Acceptance Test Feature 2: Testing for arg_ratio
+        mock_ratio.return_value = "1.33"
+
         sys.argv = ['command_line.py','--ratio','Japan']
-        self.assertEqual(self.sys_helper(), "9.602605207949379e-05")
+        self.assertEqual(self.sys_helper(), "1.33")
 
+    @patch('command_line.core.average')
+    def test_main_arg_average(self, mock_average):
+        '''Arguments: self, mock_average
+        Return value: None
+        Purpose: Testing the return of the correct value for the
+        arg_average argument
+        '''
         #Testing for arg_average
-        sys.argv = ['command_line.py', '--average', 'Canada']
-        self.assertEqual(self.sys_helper(), '148.5173125')
+        mock_average.return_value = "148.516"
 
-        #Testing for arg_biofuel
+        sys.argv = ['command_line.py', '--average', 'Canada']
+        self.assertEqual(self.sys_helper(), '148.516')
+
+    @patch('command_line.core.highest_biofuel')
+    def test_main_arg_biofuel(self, mock_biofuel):
+        '''Arguments: self, mock_biofuel
+        Return value: None
+        Purpose: Tests the highest_biofuel argument for corrrect output
+        '''
+        #Acceptance Test Feature 1: Testing for arg_biofuel
+        mock_biofuel.return_value = "33.705"
         sys.argv = ['command_line.py', '--biofuel', 'Canada']
         self.assertEqual(self.sys_helper(), '33.705')
 
-        #Testing for arg year_co2
-        # sys.argv = ['command_line.py', '--year_co2', '1998']
-        # self.assertEqual(self.sys_helper(),
-        # 'Annual CO2 emissions (measured in million tonnes) in the year 2019: Argentina: 1.609')
-
-        #Testing for no arg
-        sys.argv = ['command_line.py']
-        self.assertEqual(self.sys_helper(), "Usage: python3 command_line.py [--help]")
-
-    def test_arg_year_co2(self):
-        '''Arguments: self (TestProductionCode)
+    @patch('command_line.core.year_co2')
+    def test_main_arg_year_co2(self, mock_year_co2):
+        '''Arguments: self, mock_year_co2
         Return value: None
         Purpose: Tests if year_co2 argument in main displays correct output
         '''
+        #Acceptance Test Feature 3: Testing for arg year_co2
+        mock_year_co2.return_value = "Canada,1998,2.2"
         sys.argv = ['command_line.py', '--year_co2', '1998']
-        self.assertEqual(self.sys_helper(),
-        'Annual CO2 emissions (measured in million tonnes) in the year 2019: Argentina: 1.609')
+        self.assertEqual(self.sys_helper(), "Canada,1998,2.2")
 
-
+    def test_no_arg(self):
+        '''Argument: self
+        Return Value: None
+        Purpose: Tests if the help usage statement is printed
+        '''
+        #Testing for no arg
+        sys.argv = ['command_line.py']
+        self.assertEqual(self.sys_helper(), "Usage: python3 command_line.py [--help]")
 
     if __name__ == '__main__':
         unittest.main()
