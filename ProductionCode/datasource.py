4a2db5ad16612dd5eb596433d08b98e4b3a98725
Rimona Zhang <zhangr2@carleton.edu>
2026-03-05 19:41:54 -0500

    added back variable

--- ProductionCode/datasource.py
+++ ProductionCode/datasource.py
@@ -43,8 +43,8 @@ class DataSource:
         Return: A csv
         '''
         result = self.db.query(
-            f"""SELECT country, year, gas_electricity, 
-            energy_per_capita FROM energy_data WHERE year = '2000'"""
+            f"""SELECT country, year, gas_electricity,
+            energy_per_capita FROM energy_data WHERE year = '{year}'"""
         )
         return result.export('csv')
 
